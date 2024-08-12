20636fa322806a6d3b91fa249a44553ab86d2680
ranjan as <ranjanashish654@gmail.com>
2024-08-12 11:25:00 +0530

    Add project files.

---
+++ DatabaseRelated/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DatabaseRelated.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        //Add models for Create/Delete/Update of Table
+        DbSet<Student> Student { get; set; }
+    }
+}
---
+++ DatabaseRelated/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseRelated.Models
+{
+    public class Student
+    {
+        [Key]
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
