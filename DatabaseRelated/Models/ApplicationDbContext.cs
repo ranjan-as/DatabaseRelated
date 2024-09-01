038b7ba8b390451aa7eb6948a01eeb3ee4bd7a8a
ranjan as <ranjanashish654@gmail.com>
2024-09-01 22:55:09 +0530

    binding of data without using model

--- DatabaseRelated/Models/ApplicationDbContext.cs
+++ DatabaseRelated/Models/ApplicationDbContext.cs
@@ -13,9 +13,6 @@ namespace DatabaseRelated.Models
         {
         }
 
-        //Add models for Create/Delete/Update of Table
-        //DbSet<Student> Student { get; set; }
-
         public System.Data.Entity.DbSet<DatabaseRelated.Models.Student> Students { get; set; }
         public System.Data.Entity.DbSet<DatabaseRelated.Models.Degree> Degrees { get; set; }
 
